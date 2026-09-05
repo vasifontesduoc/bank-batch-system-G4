d121edf8b8a1e5c3ed2acb053fb9b97b85d65398
Valeria Sifontes <va.sifontes@duocuc.cl>
2026-09-04 23:43:20 -0400

    Delete bank-batch-system directory

--- bank-batch-system/src/main/java/com/bancoxyz/bank_batch_system/repository/TransaccionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bancoxyz.bank_batch_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bancoxyz.bank_batch_system.model.Transaccion;
-
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
