cf771f677d218cea76c0c65becfeb666aa78862a
Valeria Sifontes <va.sifontes@duocuc.cl>
2026-09-04 23:43:07 -0400

    Delete bank-batch-system-2 directory

--- bank-batch-system-2/src/main/java/com/bancoxyz/bank_batch_system/repository/CuentaAnualRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancoxyz.bank_batch_system.repository;
-
-import com.bancoxyz.bank_batch_system.model.CuentaAnual;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuentaAnualRepository extends JpaRepository<CuentaAnual, Long> {
-}
--- bank-batch-system-2/src/main/java/com/bancoxyz/bank_batch_system/repository/InteresRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancoxyz.bank_batch_system.repository;
-
-import com.bancoxyz.bank_batch_system.model.Interes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InteresRepository extends JpaRepository<Interes, Long> {
-}
